c760b8ecc2c71365109edd60cc3f5729dd1b02ff
PotcFdk <PotcFdk@gmx.net>
2026-01-03 17:46:39 +0100

    Update meta.js files with the new generator.

--- HeiseThreadEnabler/HeiseThreadEnabler.meta.js
+++ HeiseThreadEnabler/HeiseThreadEnabler.meta.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/PotcFdk
 // @description Auto-expands the thread view in the forums / comment sections.
 // @match       https://www.heise.de/forum/heise-online/Kommentare/*/posting-*/show/
-// @version     0.0.0
+// @version     0.0.1
 // @grant       none
 // @icon        https://avatars.githubusercontent.com/u/8555894
 // @homepageURL https://github.com/PotcFdk/userscripts/tree/master/HeiseThreadEnabler
